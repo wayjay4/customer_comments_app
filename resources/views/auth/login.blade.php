782f3eaa4b3a5517800198aa8399dc77f6f6de7c
Waylon Dixon <wayjay4@yahoo.com>
2021-06-13 20:19:02 -0700

    added register link to login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -33,6 +33,11 @@
             </div>
 
             <div class="flex items-center justify-end mt-4">
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}" class="underline text-sm text-gray-600 hover:text-gray-900">Need to Register?</a>
+                @endif
+                &nbsp;
+                &nbsp;
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
